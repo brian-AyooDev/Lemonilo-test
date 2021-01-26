f245f48228348c19c0a9d6174db8697f1549c90e
Bryn <brian.handias@gmail.com>
2021-01-26 14:03:43 +0700

    Update style Splashscreen.js

--- App/Pages/Splash.js
+++ App/Pages/Splash.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import {
   ActivityIndicator,
+  Dimensions,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import * as Styles from 'App/Styles';
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: Styles.Colors.primary,
   }
 });
 
@@ -18,14 +23,21 @@ class Splash extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.props.navigation.navigate('Home');
-    }, 3500);
+    }, 800);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Splash Screen</Text>
-        <ActivityIndicator />
+        <View>
+          <Text style={[
+            Styles.Helpers.center,
+            { fontSize: 32, fontWeight: 'bold', color: Styles.Colors.white }
+          ]}>
+            {'PropertyApp'}
+          </Text>
+          <ActivityIndicator style={{ marginTop: width * 0.25 }} />
+        </View>
       </View>
     );
   }
